e378da26cab55fd99f2144537f9f1b393f6fcc44
grandhisreephanikiran <110846283+grandhisreephanikiran@users.noreply.github.com>
2022-08-25 22:30:17 +0530

    Create unitiled 1.c

---
+++ unitiled 1.c
@@ -0,0 +1,11 @@
+ #include<stdio.h>
+int  main()
+{
+	char a[10],count,str,n=3;
+	printf("enter string");
+	scanf("%s [I love India]a");
+	if(str[a]=='n')
+	count++;
+	printf("%s",a);
+	return 0;
+}
